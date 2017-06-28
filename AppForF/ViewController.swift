143dccb69865d0225e29ae44dd6124f6047f8aa4
AlexanderYakovenko <AlexanderYakovenko@Hellens-Mac-mini.local>
2017-06-28 09:24:50 +0300

    first commit

--- AppForF/ViewController.swift
+++ AppForF/ViewController.swift
@@ -7,11 +7,18 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
+    
+    var dict : [String : AnyObject]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("ok")
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,6 +26,42 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func btnFBLoginPresssed(_ sender: Any) {
+        
+        let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if (error == nil){
+                let fbloginresult : FBSDKLoginManagerLoginResult = result!
+                if fbloginresult.grantedPermissions != nil {
+                    if(fbloginresult.grantedPermissions.contains("email"))
+                    {
+                        self.getFBUserData()
+                        fbLoginManager.logOut()
+                    }
+                }
+            }
+        }
+        
+        
+        
+        
+        
+    }
+    
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(result!)
+                    print(self.dict)
+                }
+            })
+        }
+    }
+    
 
 
 }
